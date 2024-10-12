a6f131b835fd2d350a7d896dc19178f4ef9511e6
Astra <abhaypratapsinghwd@gmail.com>
2024-10-13 02:13:52 +0530

    Improved UI for Connect Github Button

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -13,14 +13,25 @@ const SignInPage = () => {
           <h1 className="text-lg font-medium">{siteConfig.name}</h1>
         </Link>
       </div>
-      <div className="flex flex-1 items-center justify-center flex-col gap-2">
+      <div className="flex mt-16 mb-8 items-center justify-center flex-col gap-2 w-full">
         <form
           action={async () => {
             "use server";
             await signIn("github");
           }}
+          className="w-full px-8"
         >
-          <button type="submit">Sign-in with GitHub</button>
+          <button
+            type="submit"
+            className="relative inline-flex h-10 overflow-hidden rounded-full p-[1.5px] w-full"
+          >
+            <span className="absolute inset-[-1000%] animate-[spin_3s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,hsl(var(--primary))_0%,hsl(var(--primary-foreground))_50%,hsl(var(--primary))_100%)]" />
+
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-[hsl(var(--background))] px-4 py-1 text-sm font-medium text-[hsl(var(--foreground))] backdrop-blur-3xl">
+              Connect Github
+              <Icons.next className="ml-2 size-6 animate-moveLeftRight" />
+            </span>
+          </button>
         </form>
       </div>
       <div className="flex flex-col items-start w-full">
